787cd76cb4e301f2f0f655efde2236a87cac67e0
Feuerbach, Jacob D <jacob.feuerbach@gmail.com>
2019-06-24 16:01:40 -0500

    Using component from material schematic

--- projects/uiowa/uiowa-header-mat/src/lib/uiowa-header-mat.module.ts
+++ projects/uiowa/uiowa-header-mat/src/lib/uiowa-header-mat.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from "@angular/core";
 import { UiowaHeaderMatComponent } from "./uiowa-header-mat/uiowa-header-mat.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatToolbarModule } from "@angular/material";
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from "@angular/material";
 import { UiowaBrandingComponent } from "./uiowa-branding/uiowa-branding.component";
 import { UiowaAppNavBarComponent } from "./uiowa-app-nav-bar/uiowa-app-nav-bar.component";
+import { UiowaNavComponent } from './uiowa-nav/uiowa-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     UiowaHeaderMatComponent,
     UiowaBrandingComponent,
-    UiowaAppNavBarComponent
+    UiowaAppNavBarComponent,
+    UiowaNavComponent
   ],
-  imports: [BrowserAnimationsModule, MatToolbarModule],
+  imports: [BrowserAnimationsModule, MatToolbarModule, LayoutModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
   exports: [UiowaHeaderMatComponent, MatToolbarModule]
 })
 export class UiowaHeaderMatModule {}
---
+++ projects/uiowa/uiowa-header-mat/src/lib/uiowa-nav/uiowa-nav.component.spec.ts
@@ -0,0 +1,42 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+import { UiowaNavComponent } from './uiowa-nav.component';
+
+describe('UiowaNavComponent', () => {
+  let component: UiowaNavComponent;
+  let fixture: ComponentFixture<UiowaNavComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [UiowaNavComponent],
+      imports: [
+        NoopAnimationsModule,
+        LayoutModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiowaNavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
